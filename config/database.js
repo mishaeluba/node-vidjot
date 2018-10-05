3834c0d0a6e4ad88e1a4a1ccb3079b5d44b83e07
uba mishael <ubamishael@gmail.com>
2018-10-05 01:00:39 +0100

    reconfigured db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = {mongoURI: "mongodb://blinkz:{uba8080}@ds123173.mlab.com:23173/vidjot-prod"}
+  module.exports = {mongoURI: "mongodb://blinkz:uba8080@ds123173.mlab.com:23173/vidjot-prod"}
 } else {
   module.exports = {mongoURI: "mongodb://127.0.0.1:27017/vidjot-dev"}
 }
